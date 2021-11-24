41bc75753335cb67f9c76703b5ce3e9fc5fd5390
Kira Nim <kiras.nim@gmail.com>
2021-11-24 19:07:17 +0100

    Create a projects router and export it to app - make backend module for project endpoints.

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
-// Import express 
+// Import express module
 const express = require('express') 
 
+// Import the module object from which it is possible to get a config obj made in the projects.js file
+// This can be used to get acces to an endpoint defined in the projects.js file
+const projectsRouter = require("/routers/projects.js");
 
 // call the library to get an app obj which will be used to configure the server
 const app = express() 
@@ -8,19 +11,26 @@ const app = express()
 // var with value port number. Find portnumber in invironment or use 8080 as default.
 const PORT = process.env.PORT || 8080
 
-// 
+
+// express.static("public") 
+// --> returns a component that can take care of serving given static files to client.
+// app.use() 
+// --> configure the server to use the above mentioned component.
 app.use(express.static("public"));
+
+// Configure the server to use the config obj made in the projects.js 
+// which can be used to get acces to an endpoint defined in the projects.js
+app.use(projectsRouter.router);
 
 // Register endpoint for frontpage.html
 app.get("/", (req, res) => {
-    res.sendFile(__dirname + "/frontpage/frontpage.html");
+    res.sendFile(__dirname + "/public/frontpage/frontpage.html");
 });
 
 // Register endpoint for projects.html
 app.get("/projects", (req, res) => {
     res.sendFile(__dirname + "/public/projects/projects.html");
 });
-
 
 /* Register what port the server should be listening on and open it.
     Install "cross-env" in package.json as dependensy. 
--- routers/projects.js
+++ routers/projects.js
@@ -1,3 +1,7 @@
+// Import module express and call method Router() on it
+// The value stored in router is a configuration object that will be used 
+// by the app configuration object in app.js file, to set up this file so that it can contain endpoints.
+const router = require("express").Router();
 
 // Array of js objects containing hardcoded data that should later come from db.
 const projects = [
@@ -6,6 +10,13 @@ const projects = [
     { name: "Adventure XP", category: "Java", technologies: ["Java", "Thymeleaf", "CSS", "MySQL"] }
 ];
 
-app.get("/projects", (req, res) => {
-    res.send({ projects: projects });
-}); 
+// endpoint 
+router.get("/api/projects", (req, res) => {
+    res.send({ projects });
+});
+
+// Set export att. on module to contain the router config obj. from above.
+// This is what can be accesed with "require" from other js files.
+module.exports = {
+    router
+};
