265121c51564ba435efd76f5294764ca6d2cf7d0
Kira Nim <kiras.nim@gmail.com>
2021-11-24 20:21:30 +0100

    no message

---
+++ public/global/escapeHTML.js
@@ -0,0 +1,11 @@
+
+// For user input handling/processing
+function escapeHTML(string) {
+    string = string.replace(/&/g, "&amp;");
+    string = string.replace(/>/g, "&gt;");
+    string = string.replace(/</g, "&lt;");
+    string = string.replace(/"/g, "&quot;");
+    string = string.replace(/'/g, "&#039;");
+    
+    return string;
+}
