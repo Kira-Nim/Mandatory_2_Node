9a6fc49aa83b62e815ceaaef7540f415e265f220
Kira Nim <kiras.nim@gmail.com>
2021-11-24 14:01:23 +0100

    initial setup

---
+++ app.js
@@ -0,0 +1,17 @@
+// Import express 
+const express = require('express') 
+
+// call the library to get an app obj which will be used to configure the server
+const app = express() 
+
+// var with value port number. Find portnumber in invironment or use 8080 as default.
+const PORT = process.env.PORT || 8080
+
+/* Register what port the server should be listening on and open it.
+    Install "cross-env" in package.json as dependensy. 
+    (dependency and not dev-dep because it is valueble for the costumers system administrator,
+    who is going to deploy notefolio to access a env-variable instead of using a hardcoded portnumber.*/
+app.listen(PORT, (error) => {
+    console.log(`app listening at http://localhost:${PORT}`)
+})
+
